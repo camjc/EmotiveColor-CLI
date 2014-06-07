956ec976896d84a22070ff917ebc7d2e2c3ea151
Cameron Chamberlain <hi@camjc.com>
2014-06-07 18:41:12 +1000

    images and css flexbox for about page

--- app/router.js
+++ app/router.js
@@ -3,6 +3,12 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.resource('about', { path: '/' });
+  this.resource('post', { path: '/post/:post_id' });
+  this.resource('post.new', { path: '/post/new' });
+  this.resource('emotions');
+  this.resource('emotion', { path: '/emotion/:emotion' });
+  this.resource('users');
 });
 
 export default Router;
---
+++ app/views/about.js
@@ -0,0 +1,8 @@
+var AboutView = Ember.View.extend({
+  templateName: 'about',
+  // didInsertElement: function () {
+  //     this.$("#fittext").fitText(0.62); //Problematic in translations
+  // }
+});
+
+export default AboutView;
